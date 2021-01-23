1d03bb0f4df72aa004ac7e5a5e376dfaf2512be4
浩杰 谢 <793048004@qq.com>
2021-01-23 15:29:03 +0800

    1.3更新着色器代码

---
+++ FirstOpenGL/FirstOpenGL/test.cpp
@@ -0,0 +1,145 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "Shader.h"
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+int main()
+{
+	// 实例化GLFW窗口
+	glfwInit();//glfw初始化
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//主版本号
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//次版本号
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	//（宽，高，窗口名）返回一个GLFWwindow类的实例：window
+	if (window == NULL)
+	{
+		// 生成错误则输出错误信息
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// 告诉GLFW我们希望每当窗口调整大小的时候调用改变窗口大小的函数
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// glad管理opengl函数指针，初始化glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		// 生成错误则输出错误信息
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	Shader ourShader("vertexSource.txt", "fragmentSource.txt");
+
+	////顶点数据
+	//float vertices[] = {
+	//	-0.5f, 0.5f, 0.0f,   // 3号点
+	//	-0.5f, 0.0f, 0.0f,   // 3号点
+	//	0.0f, 0.0f, 0.0f,   // 3号点
+	//	0.5f, 0.0f, 0.0f,   // 3号点
+	//	0.5f, 0.5f, 0.0f,   // 3号点
+	//};
+	//unsigned int indices[] = { // 注意索引从0开始!
+	//	0, 1, 2, // 第一个三角形
+	//	2, 3, 4  // 第二个三角形
+	//};
+	GLfloat vertices[] = {
+		// 位置              // 颜色
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+	};
+
+	unsigned int indices[] = { // 注意索引从0开始!
+		0, 1, 2, // 第一个三角形
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	// 初始化代码
+	// 1. 绑定顶点数组对象
+	glBindVertexArray(VAO);
+	// 2. 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	// 4. 设定顶点属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	//线框模式wireframe
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+	// 渲染循环
+	while (!glfwWindowShouldClose(window))
+	{
+		
+		// 输入
+		processInput(window);
+
+		// 渲染指令
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ourShader.use();
+		glBindVertexArray(VAO);
+		// 渲染循环中		
+		// 更新uniform
+		float timeValue = glfwGetTime();
+		float moveValue;
+		moveValue = sin(timeValue)/2.0f;
+		ourShader.setFloat("xOffset", moveValue);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// 检查并调用事件，交换缓冲
+		glfwSwapBuffers(window);
+
+		// 检查触发什么事件，更新窗口状态
+		glfwPollEvents();
+	}
+
+	// 释放之前的分配的所有资源
+	glfwTerminate();
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// 每当窗口改变大小，GLFW会调用这个函数并填充相应的参数供你处理
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	// 返回这个按键是否正在被按下
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)//是否按下了返回键
+		glfwSetWindowShouldClose(window, true);
+}
