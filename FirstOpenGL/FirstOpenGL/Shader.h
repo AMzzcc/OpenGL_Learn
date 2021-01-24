d64be333a0d5fd37da3444143017b3e816182a13
浩杰 谢 <793048004@qq.com>
2021-01-24 11:50:25 +0800

    1.5旋转的笑脸

--- FirstOpenGL/FirstOpenGL/Shader.h
+++ FirstOpenGL/FirstOpenGL/Shader.h
@@ -2,7 +2,9 @@
 #define SHADER_H
 
 #include <glad/glad.h>; // 包含glad来获取所有的必须OpenGL头文件
-
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -89,47 +91,47 @@ public:
 	{
 		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 	}
-	//void setVec2(const std::string& name, const glm::vec2& value) const
-	//{
-	//	glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-	//}
-	//void setVec2(const std::string& name, float x, float y) const
-	//{
-	//	glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
-	//}
-	//// ------------------------------------------------------------------------
-	//void setVec3(const std::string& name, const glm::vec3& value) const
-	//{
-	//	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-	//}
-	//void setVec3(const std::string& name, float x, float y, float z) const
-	//{
-	//	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
-	//}
-	//// ------------------------------------------------------------------------
-	//void setVec4(const std::string& name, const glm::vec4& value) const
-	//{
-	//	glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-	//}
-	//void setVec4(const std::string& name, float x, float y, float z, float w)
-	//{
-	//	glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
-	//}
-	//// ------------------------------------------------------------------------
-	//void setMat2(const std::string& name, const glm::mat2& mat) const
-	//{
-	//	glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
-	//}
-	//// ------------------------------------------------------------------------
-	//void setMat3(const std::string& name, const glm::mat3& mat) const
-	//{
-	//	glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
-	//}
-	//// ------------------------------------------------------------------------
-	//void setMat4(const std::string& name, const glm::mat4& mat) const
-	//{
-	//	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
-	//}
+	void setVec2(const std::string& name, const glm::vec2& value) const
+	{
+		glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+	void setVec2(const std::string& name, float x, float y) const
+	{
+		glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+	}
+	// ------------------------------------------------------------------------
+	void setVec3(const std::string& name, const glm::vec3& value) const
+	{
+		glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+	void setVec3(const std::string& name, float x, float y, float z) const
+	{
+		glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+	}
+	// ------------------------------------------------------------------------
+	void setVec4(const std::string& name, const glm::vec4& value) const
+	{
+		glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+	void setVec4(const std::string& name, float x, float y, float z, float w)
+	{
+		glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+	}
+	// ------------------------------------------------------------------------
+	void setMat2(const std::string& name, const glm::mat2& mat) const
+	{
+		glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+	// ------------------------------------------------------------------------
+	void setMat3(const std::string& name, const glm::mat3& mat) const
+	{
+		glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+	// ------------------------------------------------------------------------
+	void setMat4(const std::string& name, const glm::mat4& mat) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
 
 private:
 	// 检查编译或链接是否出错
