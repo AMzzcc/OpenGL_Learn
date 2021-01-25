9c20efcb0fecd50ee06064483d2a9f8d93e3aef2
浩杰 谢 <793048004@qq.com>
2021-01-25 18:04:08 +0800

    1.6坐标变换+1.7摄像机

---
+++ FirstOpenGL/FirstOpenGL/1.6_3D.cpp
@@ -0,0 +1,405 @@
+//#define STB_IMAGE_IMPLEMENTATION
+//#include "stb_image.h"
+//#include <glad/glad.h>
+//#include <GLFW/glfw3.h>
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+//#include <iostream>
+//#include "Shader.h"
+//using namespace std;
+//
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//void processInput(GLFWwindow* window);
+//void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+//
+//// settings
+//const unsigned int SCR_WIDTH = 800;
+//const unsigned int SCR_HEIGHT = 600;
+//
+////摄像机
+//glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+//glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+//glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+//
+//GLfloat fov = 45.0f;
+//bool firstMouse = true;
+//float yaw = -90.0f;
+//float pitch = 0.0f;
+//GLfloat lastX = 400, lastY = 300;
+//
+//
+//GLfloat deltaTime = 0.0f; // 当前帧与上一帧的时间差
+//GLfloat lastFrame = 0.0f; // 上一帧的时间
+//
+//
+//
+//int main()
+//{
+//#pragma region 窗口
+//	// 实例化GLFW窗口
+//	glfwInit();//glfw初始化
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//主版本号
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//次版本号
+//	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//
+//	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+//	//（宽，高，窗口名）返回一个GLFWwindow类的实例：window
+//	if (window == NULL)
+//	{
+//		// 生成错误则输出错误信息
+//		std::cout << "Failed to create GLFW window" << std::endl;
+//		glfwTerminate();
+//		return -1;
+//	}
+//	glfwMakeContextCurrent(window);
+//
+//	// 告诉GLFW我们希望每当窗口调整大小的时候调用改变窗口大小的函数
+//	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+//	glfwSetCursorPosCallback(window, mouse_callback);
+//	glfwSetScrollCallback(window, scroll_callback);
+//#pragma endregion GLFW
+//
+//#pragma region 函数指针
+//	// glad管理opengl函数指针，初始化glad
+//	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+//	{
+//		// 生成错误则输出错误信息
+//		std::cout << "Failed to initialize GLAD" << std::endl;
+//		return -1;
+//	}
+//#pragma endregion GLAD
+//
+//
+//	Shader ourShader("1.6vertex.txt", "1.6fragment.txt");
+//
+//
+//#pragma region 顶点数据
+//	float vertices[] = {
+//	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+//	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+//	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+//	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+//	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+//	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+//
+//	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+//	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+//	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+//	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+//	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+//	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+//
+//	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+//	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+//	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+//	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+//	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+//	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+//
+//	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+//	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+//	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+//	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+//	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+//	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+//
+//	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+//	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+//	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+//	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+//	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+//	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+//
+//	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+//	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+//	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+//	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+//	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+//	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+//	};
+//
+//	glm::vec3 cubePositions[] = {
+//	glm::vec3(0.0f,  0.0f,  0.0f),
+//	glm::vec3(2.0f,  5.0f, -15.0f),
+//	glm::vec3(-1.5f, -2.2f, -2.5f),
+//	glm::vec3(-3.8f, -2.0f, -12.3f),
+//	glm::vec3(2.4f, -0.4f, -3.5f),
+//	glm::vec3(-1.7f,  3.0f, -7.5f),
+//	glm::vec3(1.3f, -2.0f, -2.5f),
+//	glm::vec3(1.5f,  2.0f, -2.5f),
+//	glm::vec3(1.5f,  0.2f, -1.5f),
+//	glm::vec3(-1.3f,  1.0f, -1.5f)
+//	};
+//
+//#pragma endregion vertices[],indices[]
+//
+//
+//#pragma region 缓存对象
+//	// 初始化缓存对象
+//	unsigned int VBO;
+//	glGenBuffers(1, &VBO);
+//	unsigned int VAO;
+//	glGenVertexArrays(1, &VAO);
+//
+//	// 1. 绑定顶点数组对象
+//	glBindVertexArray(VAO);
+//	// 2. 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
+//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//
+//	// 4. 设定顶点属性指针
+//	// 位置属性
+//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+//	glEnableVertexAttribArray(0);
+//	// 纹理坐标属性
+//	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+//	glEnableVertexAttribArray(1);
+//
+//	glBindVertexArray(0); // Unbind VAO
+//#pragma endregion VAO，VBO，EBO
+//
+//	//线框模式wireframe
+//	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//
+//#pragma region 材质
+//	unsigned int texture1, texture2;//纹理也是使用ID引用的
+//// texture1
+//	glGenTextures(1, &texture1);//glGenTextures先输入要生成纹理的数量，然后把它们储存在第二个参数的`unsigned int`数组中
+//	glBindTexture(GL_TEXTURE_2D, texture1);
+//	// 为当前绑定的纹理对象设置环绕、过滤方式
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//	//float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+//	//glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+//	// 加载并生成纹理
+//	int width, height, nrChannels;
+//	stbi_set_flip_vertically_on_load(true);
+//	unsigned char* data = stbi_load("face.png", &width, &height, &nrChannels, 0);
+//	if (data)
+//	{
+//		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+//		glGenerateMipmap(GL_TEXTURE_2D);
+//	}
+//	else
+//	{
+//		std::cout << "Failed to load texture" << std::endl;
+//	}
+//	stbi_image_free(data);
+//	// texture2
+//	glGenTextures(1, &texture2);
+//	glBindTexture(GL_TEXTURE_2D, texture2);
+//	// 为当前绑定的纹理对象设置环绕、过滤方式
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//	// 加载并生成纹理
+//	data = stbi_load("container.png", &width, &height, &nrChannels, 0);
+//	if (data)
+//	{
+//		// note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+//		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+//		glGenerateMipmap(GL_TEXTURE_2D);
+//	}
+//	else
+//	{
+//		std::cout << "Failed to load texture" << std::endl;
+//	}
+//	stbi_image_free(data);
+//#pragma endregion 加载材质
+//
+//	
+//
+//	ourShader.use(); // 别忘记在激活着色器前先设置uniform！
+//	
+//	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0); // 手动设置
+//	ourShader.setInt("texture2", 1); // 或者使用着色器类设置
+//
+//	glEnable(GL_DEPTH_TEST);
+//
+//	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+//
+//
+//	// 渲染循环
+//	while (!glfwWindowShouldClose(window))
+//	{
+//
+//		GLfloat currentFrame = glfwGetTime();
+//		deltaTime = currentFrame - lastFrame;
+//		lastFrame = currentFrame;
+//		// 输入
+//		processInput(window);
+//
+//
+//		// 渲染指令
+//		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+//		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//
+//		// 绑定材质
+//		/*glBindTexture(GL_TEXTURE_2D, texture1);*/
+//
+//		// 绑定材质
+//		glActiveTexture(GL_TEXTURE0);
+//		glBindTexture(GL_TEXTURE_2D, texture1);
+//		glActiveTexture(GL_TEXTURE1);
+//		glBindTexture(GL_TEXTURE_2D, texture2);
+//
+//		// 渲染循环中		
+//		// 更新uniform
+//		/*float timeValue = glfwGetTime();
+//		float moveValue;
+//		moveValue = sin(timeValue)/2.0f;
+//		ourShader.setFloat("xOffset", moveValue);*/
+//
+//	//	float timeValue = glfwGetTime();
+//	//	float scaleValue = sin(timeValue) / 2.0f;
+//	//	glm::mat4 trans; // 单位矩阵
+//	//// 传递单位矩阵和一个位移向量
+//	//	trans = glm::translate(trans, glm::vec3(scaleValue, -scaleValue, 0.0f));
+//	//	trans = glm::rotate(trans, (float)timeValue, glm::vec3(0.0, 0.0, 1.0));
+//	//	trans = glm::scale(trans, glm::vec3(scaleValue, scaleValue, scaleValue));
+//	//	unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+//	//	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+//
+//		/*glm::mat4 model;
+//		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));*/
+//		//glm::mat4 view;
+//		//// 注意，我们将矩阵向我们要进行移动场景的反向移动。
+//		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+//		//ourShader.setMat4("view", view);
+//
+//		glm::mat4 projection;
+//		projection = glm::perspective(glm::radians(fov), float(SCR_WIDTH / SCR_HEIGHT), 0.1f, 100.0f);
+//		// 将矩阵传入着色器
+//		/*ourShader.setMat4("model", model);*/
+//		ourShader.setMat4("projection", projection);
+//
+//		////摄像机旋转
+//		//GLfloat radius = 10.0f;
+//		//GLfloat camX = sin(glfwGetTime()) * radius;
+//		//GLfloat camZ = cos(glfwGetTime()) * radius;
+//		//glm::mat4 view;
+//		//view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+//
+//		glm::mat4 view;
+//		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+//		ourShader.setMat4("view", view);
+//
+//		//glm::mat4 model = glm::mat4(1.0f);
+//		//glm::mat4 view = glm::mat4(1.0f);
+//		//glm::mat4 projection = glm::mat4(1.0f);
+//		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+//		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+//		//projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+//		//// 将矩阵传入着色器
+//		//ourShader.setMat4("model", model);
+//		//ourShader.setMat4("view", view);
+//		//ourShader.setMat4("projection", projection);
+//
+//		ourShader.use();
+//
+//
+//
+//		glBindVertexArray(VAO);
+//		for (unsigned int i = 0; i < 10; i++)
+//		{
+//			glm::mat4 model;
+//			model = glm::translate(model, cubePositions[i]);
+//			float angle = 20.0f * i;
+//			//if (i % 3 == 0)  // every 3rd iteration (including the first) we set the angle using GLFW's time function.
+//				angle = glfwGetTime() * 25.0f;
+//			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+//			ourShader.setMat4("model", model);
+//
+//			glDrawArrays(GL_TRIANGLES, 0, 36);
+//		}
+//		glBindVertexArray(0);
+//
+//
+//
+//		// 检查并调用事件，交换缓冲
+//		glfwSwapBuffers(window);
+//
+//		// 检查触发什么事件，更新窗口状态
+//		glfwPollEvents();
+//	}
+//
+//	// 释放之前的分配的所有资源
+//	glDeleteVertexArrays(1, &VAO);
+//	glDeleteBuffers(1, &VBO);
+//	glfwTerminate();
+//
+//	return 0;
+//}
+//
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+//{
+//	// 每当窗口改变大小，GLFW会调用这个函数并填充相应的参数供你处理
+//	glViewport(0, 0, width, height);
+//}
+//
+//void processInput(GLFWwindow* window)
+//{
+//
+//	// 返回这个按键是否正在被按下
+//	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)//是否按下了返回键
+//		glfwSetWindowShouldClose(window, true);
+//
+//	GLfloat cameraSpeed = 2.5f * deltaTime;
+//	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+//		cameraPos += cameraSpeed * cameraFront;
+//	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+//		cameraPos -= cameraSpeed * cameraFront;
+//	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+//		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+//	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+//		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+//}
+//
+//void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+//{
+//	//
+//	if (firstMouse)
+//	{
+//		lastX = xpos;
+//		lastY = ypos;
+//		firstMouse = false;
+//	}
+//
+//	//第一步是计算鼠标自上一帧的偏移量，sensitivity（灵敏度）值，如果我们忽略这个值，鼠标移动就会太大了
+//	float xoffset = xpos - lastX;
+//	float yoffset = lastY - ypos; // 注意这里是相反的，因为y坐标是从底部往顶部依次增大的
+//	lastX = xpos;
+//	lastY = ypos;
+//
+//	float sensitivity = 0.05f;
+//	xoffset *= sensitivity;
+//	yoffset *= sensitivity;
+//	yaw += xoffset;
+//	pitch += yoffset;
+//	//第三步，我们需要给摄像机添加一些限制
+//	if (pitch > 89.0f)
+//		pitch = 89.0f;
+//	if (pitch < -89.0f)
+//		pitch = -89.0f;
+//	//第四也是最后一步，就是通过俯仰角和偏航角来计算以得到真正的方向向量：
+//	glm::vec3 front;
+//	front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+//	front.y = sin(glm::radians(pitch));
+//	front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+//	cameraFront = glm::normalize(front);
+//}
+//
+//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+//{
+//	if (fov >= 1.0f && fov <= 45.0f)
+//		fov -= yoffset;
+//	if (fov <= 1.0f)
+//		fov = 1.0f;
+//	if (fov >= 45.0f)
+//		fov = 45.0f;
+//}
---
+++ FirstOpenGL/FirstOpenGL/1.7.cpp
@@ -0,0 +1,321 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader.h"
+#include "camera.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// callback
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f; // 当前帧与上一帧的时间差
+float lastFrame = 0.0f; // 上一帧的时间
+
+int main()
+{
+
+#pragma region 窗口
+	// 实例化GLFW窗口
+	glfwInit();//glfw初始化
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//主版本号
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//次版本号
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	//（宽，高，窗口名）返回一个GLFWwindow类的实例：window
+	if (window == NULL)
+	{
+		// 生成错误则输出错误信息
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// 告诉GLFW我们希望每当窗口调整大小的时候调用改变窗口大小的函数
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+#pragma endregion GLFW
+
+#pragma region 函数指针
+	// glad管理opengl函数指针，初始化glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		// 生成错误则输出错误信息
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+#pragma endregion GLAD
+
+	Shader ourShader("1.6vertex.txt", "1.6fragment.txt");
+
+	glEnable(GL_DEPTH_TEST);//启用深度测试
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);//
+
+#pragma region 顶点数据
+//顶点数据
+	float vertices[] = {
+		//   ---- 位置 ----		  - 纹理坐标 -
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+#pragma endregion vertices[],indices[]
+
+#pragma region 缓存对象
+	// 初始化缓存对象
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	//unsigned int EBO;
+	//glGenBuffers(1, &EBO);
+	// 1. 绑定顶点数组对象
+	glBindVertexArray(VAO);
+	// 2. 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//// 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	// 4. 设定顶点属性指针
+	// 位置属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// 纹理坐标属性
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+#pragma endregion VAO，VBO，EBO
+
+#pragma region 材质
+	unsigned int texture1, texture2;//纹理也是使用ID引用的
+// texture1
+	glGenTextures(1, &texture1);//glGenTextures先输入要生成纹理的数量，然后把它们储存在第二个参数的`unsigned int`数组中
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	// 为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("face.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	// texture2
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	// 为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// 加载并生成纹理
+	data = stbi_load("container.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		// note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+#pragma endregion 加载材质
+	
+		
+
+#pragma region 渲染
+
+	ourShader.use();
+	ourShader.setInt("texture1", 0);
+	ourShader.setInt("texture2", 1); 
+
+	// 渲染循环
+	while (!glfwWindowShouldClose(window))
+	{
+		// 帧时间差
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// 输入
+		processInput(window);
+
+		// 渲染指令
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// 绑定材质
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		// 创建矩阵
+		glm::mat4 view = camera.GetViewMatrix();
+		ourShader.setMat4("view", view);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		ourShader.setMat4("projection", projection);
+
+		// 将矩阵传入着色器
+		//ourShader.setMat4("model", model);
+		// 渲染箱子
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			ourShader.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		// 检查并调用事件，交换缓冲
+		glfwSwapBuffers(window);
+
+		// 检查触发什么事件，更新窗口状态
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	//glDeleteBuffers(1, &EBO);
+
+	// 释放之前的分配的所有资源
+	glfwTerminate();
+
+	return 0;
+}
+
+#pragma endregion Rendering
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// 每当窗口改变大小，GLFW会调用这个函数并填充相应的参数供你处理
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	// 返回这个按键是否正在被按下
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)//是否按下了返回键
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
